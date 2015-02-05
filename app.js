bacbbb20b6d8243c32535b539e367980d444c5cb
rebeccalayman <rmlaym01@louisville.edu>
2015-02-04 19:21:01 -0500

    Updating paths
    This now lets the site work in Firefox.

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 (function() {
   var app = angular.module('gemStore', ['store-directives']);
 
-  app.controller('StoreController', ['$http', function($http){
-    var store = this;
-    store.products = [];
-    
-    $http.get('/store-products.json').success(function(data){
-      store.products = data;
-    });
-    
-  }]);
+app.controller('StoreController', ['$http', function($http){
+  var store = this;
+  store.products = [];
+
+  $http.get('store-products.json').success(function(data){
+    store.products = data;
+  });
+}]);
 
   app.controller('ReviewController', function() {
     this.review = {};
